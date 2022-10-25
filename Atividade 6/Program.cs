463b6a3a0b1238d9cc20a12dddc09d1ef242fed2
FabiannaFranci <dainezi.franci@gmail.com>
2022-10-25 15:46:22 -0300

    Lista se repetindo

--- Atividade 6/Program.cs
+++ Atividade 6/Program.cs
@@ -2,15 +2,16 @@
 using System.Globalization;
 using Projetos_Senai.Classes;
 
-Endereco novoEndPJ = new Endereco();
-PessoaJuridica novaPJ = new PessoaJuridica();
-PessoaJuridica metodosPJ = new PessoaJuridica();
-
-Endereco novoEndPF = new Endereco();
-PessoaFisica novaPF = new PessoaFisica();
-PessoaFisica metodosPF = new PessoaFisica();
-
 Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+string? opcoesDoMenu;
+bool dataValida;
+bool cpfValido;
+bool cnpjValido;
+string? verificarSeEnderecoEComercialPF;
+string? verificarSeEnderecoEComercialPJ;
+List<PessoaJuridica> listaPJ = new List<PessoaJuridica>();
+List<PessoaFisica> listaPF = new List<PessoaFisica>();
 
 Console.WriteLine(@$"
 ===========================================
@@ -21,12 +22,6 @@ Console.WriteLine(@$"
 Console.ResetColor();
 Util.MetodosUteis("Sistema Inicializando", 500, 5);
 //Console.Clear();
-string? opcoesDoMenu;
-bool dataValida;
-bool cpfValido;
-bool cnpjValido;
-string? verificarSeEnderecoEComercialPF;
-string? verificarSeEnderecoEComercialPJ;
 
 
 do
@@ -49,6 +44,9 @@ Thread.Sleep(500);
     switch (opcoesDoMenu)
     {
         case "1":
+         Endereco novoEndPF = new Endereco();
+        PessoaFisica novaPF = new PessoaFisica();
+        PessoaFisica metodosPF = new PessoaFisica();
         string? opcaoMenuPF;
         do
         {
@@ -64,16 +62,16 @@ Escolha uma opção abaixo
 
             switch(opcaoMenuPF){
                 case "1":
+
                 Console.Write("Digite seu nome: ");
                 novaPF.Nome = Console.ReadLine();
                 Console.Write("Digite seu CPF: ");
                 do
                 {
                     string? cpfQuestion = Console.ReadLine();
-                    cpfValido = novaPF.ValidarCpf(cpfQuestion);
+                    cpfValido = novaPF.ValidarCpf(cpfQuestion!);
                     if(cpfValido){
                         novaPF.cpf = cpfQuestion;
-                        Console.WriteLine(cpfValido);
                     }
                     else{
                         Console.WriteLine("CPF inválido, tente novamente.");
@@ -88,7 +86,7 @@ Escolha uma opção abaixo
                 do
                 {
                     string? dataNasc = Console.ReadLine();
-                    dataValida = novaPF.ValidarDataNasc(dataNasc);
+                    dataValida = novaPF.ValidarDataNasc(dataNasc!);
                     if(dataValida){
                         novaPF.DataDeNascimento = Convert.ToDateTime(dataNasc);
 
@@ -100,22 +98,22 @@ Escolha uma opção abaixo
 
                 } while (dataValida == false);
 
-                Console.Write("Digite seu rendimento mensal");
-                novaPF.Rendimento = float.Parse(Console.ReadLine());
+                Console.Write("Digite seu rendimento mensal: R$");
+                novaPF.Rendimento = float.Parse(Console.ReadLine()!);
 
-                Console.Write("Digite seu endereço:");
+                Console.Write("Digite seu endereço: ");
                 novoEndPF.Logradouro = Console.ReadLine();
 
-                Console.Write("Nº:");
+                Console.Write("Nº: ");
                 novoEndPF.Numero = Console.ReadLine();
 
-                Console.Write("Complemento:");
+                Console.Write("Complemento: ");
                 novoEndPF.Complemento = Console.ReadLine();
 
                 do
                 { 
-                    Console.Write("Comercial? [S/N]:  ");
-                    verificarSeEnderecoEComercialPF = Console.ReadLine().ToLower();
+                    Console.Write("Comercial? [S/N]: ");
+                    verificarSeEnderecoEComercialPF = Console.ReadLine()!.ToLower();
                     if(verificarSeEnderecoEComercialPF  == "s")
                     {
                         novoEndPF.EnderecoComercial = true;
@@ -129,20 +127,33 @@ Escolha uma opção abaixo
                         Console.WriteLine("Inválido, tente novamente.");
                     }
                     
-                } while (verificarSeEnderecoEComercialPF  != "s" || verificarSeEnderecoEComercialPF != "n");
+                } while (verificarSeEnderecoEComercialPF  != "s" && verificarSeEnderecoEComercialPF != "n");
 
+                novaPF.Endereco = novoEndPF;
+                listaPF.Add(novaPF);
             break;
 
                 case "2":
+                if(listaPF.Count > 0){
+                    foreach(PessoaFisica pf in listaPF){
                     Console.WriteLine(@$"
-                    Nome: {novaPF.Nome}
-                    CPF: {novaPF.cpf}
-                    Data de Nascimento: {novaPF.DataDeNascimento}
-                    Rendimento: {novaPF.Rendimento}
-                    Imposto à pagar: {metodosPF.PagarImposto(novaPF.Rendimento).ToString("C", new CultureInfo("pt-BR"))}
-                    Endereço Comercial?: {(novoEndPF.EnderecoComercial? "Sim" : "Não")}
+                    Nome: {pf.Nome}
+                    CPF: {pf.cpf}
+                    Data de Nascimento: {pf.DataDeNascimento}
+                    Rendimento: {pf.Rendimento}
+                    Imposto à pagar: {metodosPF.PagarImposto(pf.Rendimento).ToString("C", new CultureInfo("pt-BR"))}
+                    Endereço Comercial?: {(novaPF.Endereco!.EnderecoComercial? "Sim" : "Não")}
                     ");
-                            
+                    }
+                
+                } 
+                else{
+                    Console.WriteLine("Lista Vazia");
+                }
+                Console.WriteLine("Aperte ENTER para continuar");
+                Console.ReadLine();
+                
+                          
                 break;
 
                 }
@@ -151,6 +162,9 @@ Escolha uma opção abaixo
     
         case "2":
         string? opcaoMenuPJ;
+        Endereco novoEndPJ = new Endereco();
+        PessoaJuridica novaPJ = new PessoaJuridica();
+        PessoaJuridica metodosPJ = new PessoaJuridica();
         do
         {
                        Console.WriteLine(@$"
@@ -165,18 +179,21 @@ Escolha uma opção abaixo
             switch (opcaoMenuPJ)
             {
                 case "1":
+    
+
                 Console.WriteLine("Digite seu nome: ");
                 novaPJ.Nome = Console.ReadLine();
                 Console.WriteLine("Razão Social: ");
                 novaPJ.RazaoSocial = Console.ReadLine();
                 Console.WriteLine("Rendimento Mensal: ");
-                novaPJ.Rendimento = float.Parse(Console.ReadLine());
+                novaPJ.Rendimento = float.Parse(Console.ReadLine()!);
+               
                 
                 do
                 {
                     Console.WriteLine("Digite seu Cnpj: ");
                     string? cnpjQuestion = Console.ReadLine();
-                    cnpjValido = novaPJ.ValidarCnpj(cnpjQuestion);
+                    cnpjValido = novaPJ.ValidarCnpj(cnpjQuestion!);
                     if (cnpjValido == true){
                         novaPJ.Cpnj = cnpjQuestion;
                     }
@@ -184,17 +201,19 @@ Escolha uma opção abaixo
                         Console.WriteLine("Cnpj inválido, digite novamente");
                     }
                 }while (cnpjValido == false);
+
                 Console.WriteLine("Digite seu endereço: ");
                 novoEndPJ.Logradouro = Console.ReadLine();
                 Console.WriteLine("Nº: ");
                 novoEndPJ.Numero = Console.ReadLine();
                 Console.WriteLine("Complemento: ");
                 novoEndPJ.Complemento = Console.ReadLine();
+
                 do
                 {
                     
                     Console.WriteLine("Endereço é comercial?: ");
-                    verificarSeEnderecoEComercialPJ = Console.ReadLine().ToLower();
+                    verificarSeEnderecoEComercialPJ = Console.ReadLine()!.ToLower();
                     if(verificarSeEnderecoEComercialPJ == "s"){
                         novoEndPJ.EnderecoComercial = true;
                     }
@@ -206,30 +225,39 @@ Escolha uma opção abaixo
                     }
                     
                     
-                } while (verificarSeEnderecoEComercialPJ != "s"|| verificarSeEnderecoEComercialPJ  != "n");
+                } while (verificarSeEnderecoEComercialPJ != "s" && verificarSeEnderecoEComercialPJ  != "n");
                 
-                
+                novaPJ.Endereco = novoEndPJ;
 
-
-
+                listaPJ.Add(novaPJ);
                 
             break;
 
             case "2":
+            if(listaPJ.Count > 0){
+                foreach (PessoaJuridica pj in listaPJ)
+                {
+                    
                 Console.WriteLine(@$"
-                Nome da Empresa: {novaPJ.Nome}
-                Razão Social: {novaPJ.RazaoSocial}
-                Rendimento Mensal: {novaPJ.Rendimento.ToString("C", new CultureInfo("pt-BR"))}
-                Cnpj: {novaPJ.Cpnj}
-                Cnpj: {(metodosPJ.ValidarCnpj(novaPJ.Cpnj) ? "Válido" : "Inválido")}
-                Endereço: {novaPJ.Endereco.Logradouro} nº{novaPJ.Endereco.Numero}
-                Complemento: {novaPJ.Endereco.Complemento}
-                Comercial: {novaPJ.Endereco.EnderecoComercial}
-                Imposto à ser pago: {metodosPJ.PagarImposto(novaPJ.Rendimento).ToString("C", new CultureInfo("pt-BR"))}");
-
+                Nome da Empresa: {pj.Nome}
+                Razão Social: {pj.RazaoSocial}
+                Rendimento Mensal: {pj.Rendimento.ToString("C", new CultureInfo("pt-BR"))}
+                Cnpj: {pj.Cpnj}
+                Endereço: {pj.Endereco!.Logradouro} nº{pj.Endereco.Numero}
+                Complemento: {pj.Endereco.Complemento}
+                Comercial? {pj.Endereco.EnderecoComercial}
+                Imposto à ser pago: {metodosPJ.PagarImposto(pj.Rendimento).ToString("C", new CultureInfo("pt-BR"))}");
+                }
+                Console.WriteLine("Aperte ENTER para continuar");
+                Console.ReadLine();
+                
+            }
+            else{
+                Console.WriteLine("Lista vazia");
+            }
             break;
             }
-        }while (opcaoMenuPJ == "0");
+        }while (opcaoMenuPJ != "0");
         break;
         case "0":
     Console.WriteLine($"Obrigada por utilizar nosso sistema.");
